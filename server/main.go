d9f3b11c32b5e884bc35d4b33f37d2b7595c1feb
svwielga4 <svwielga4@gmail.com>
2019-10-29 09:50:49 -0700

    added server

--- server/main.go
+++ server/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
+	"sync"
 
 	"github.com/svwielga4/grpc/chat"
+	"google.golang.org/grpc"
 )
 
 // Connection struct used to communicate through
@@ -69,5 +73,79 @@ func (c *Connection) GetMessages(broadcast chan<- *chat.ChatMessage) error {
 			case <-c.quit:
 			}
 		}(msg)
+	}
+}
+
+type ChatServer struct {
+	broadcast   chan *chat.ChatMessage
+	quit        chan struct{}
+	connections []*Connection
+	connLock    sync.Mutex
+}
+
+func NewChatServer() *ChatServer {
+	srv := &ChatServer{
+		broadcast: make(chan *chat.ChatMessage),
+		quit:      make(chan struct{}),
+	}
+	go srv.start()
+	return srv
+}
+
+// Close is a global closer for all channels
+func (c *ChatServer) Close() error {
+	close(c.quit)
+	return nil
+}
+
+func (c *ChatServer) start() {
+	running := true
+	for running {
+		select {
+		case msg := <-c.broadcast:
+			c.connLock.Lock()
+			for _, v := range c.connections {
+				go v.Send(msg)
+			}
+			c.connLock.Unlock()
+		}
+	}
+}
+
+func (c *ChatServer) Chat(stream chat.Chat_ChatServer) error {
+	conn := NewConnection(stream)
+
+	c.connLock.Lock()
+	c.connections = append(c.connections, conn)
+	c.connLock.Unlock()
+
+	err := conn.GetMessages(c.broadcast)
+
+	c.connLock.Lock()
+	for i, v := range c.connections {
+		if v == conn {
+			c.connections = append(c.connections[:i], c.connections[i+1:]...)
+		}
+	}
+	c.connLock.Unlock()
+
+	return err
+}
+
+func main() {
+	lst, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+
+	srv := NewChatServer()
+	chat.RegisterChatServer(s, srv)
+
+	fmt.Println("Now serving at port 8080")
+	err = s.Serve(lst)
+	if err != nil {
+		panic(err)
 	}
 }
